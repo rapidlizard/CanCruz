e62c35188b26ddbeb9167aa30c21efe0816079f3
evalbors <evalbors@gmail.com>
2020-05-18 11:59:58 +0200

    AlenEva: first commit to Calendar

---
+++ database/factories/CalendarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Calendar;
+use Faker\Generator as Faker;
+
+$factory->define(Calendar::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

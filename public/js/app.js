c76c86f36dc2f88b1dde1696b53e8977df3f2d89
evalbors <evalbors@gmail.com>
2020-05-20 14:59:43 +0200

    Eva: create html pages in devkit: contacto, huerto_cocina, la_masia, parque and reserva.Also change name of dev, now home.tml. Create new components: header-basic, img-2col, img-text-intro, quote-text. Also continue estancia.html.

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Axel\Documents\AXEL\CODER\FACTORIAF5\GIT\CanCruz2\CanCruz\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Axel\Documents\AXEL\CODER\FACTORIAF5\GIT\CanCruz2\CanCruz\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/Factoria_F5/CanCruz/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Factoria_F5/CanCruz/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

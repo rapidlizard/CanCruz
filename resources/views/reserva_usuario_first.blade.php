cb9c5c67668be2a25881e6425c246c4d4a3a6ece
Axel <axelcayuela@hotmail.com>
2020-05-24 23:43:24 +0200

    Fix Validate dates Reserva usuario

--- resources/views/reserva_usuario_first.blade.php
+++ resources/views/reserva_usuario_first.blade.php
@@ -3,11 +3,18 @@
 @section('content')
 
     <main>
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
+    <div class="row">
+            <div class="col-md-2"></div>
+                <div class="col-12 col-md-8">
+                @if(isset($error))
+                    <div class="alert alert-danger alert-dismissible error" role="alert">
+                        <strong>{{$error}}</strong> 
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                </div>
+             @endif
         </div>
         <section>
             <div class="h2texto">
